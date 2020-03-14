1ac9b3b73f9f7f71a6a049cd2e4294d21b54dbbf
d1618033 <d1618033@gmail.com>
2020-03-14 12:38:10 +0200

    Bump version: 0.0.0 → 0.1.0

--- flake8_if_statements/plugin.py
+++ flake8_if_statements/plugin.py
@@ -5,5 +5,5 @@ from flake8_if_statements.visitor import IfStatementsVisitor
 
 class IfStatementsPlugin(Plugin):
     name = 'IfStatementsPlugin'
-    version = '0.0.0'
+    version = '0.1.0'
     visitors = [IfStatementsVisitor]
